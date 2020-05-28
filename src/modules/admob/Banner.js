c691bde673fdc4ecf849187ab4df661c0c36c873
Vladimir Panov <vladimir.panov@motorsport.com>
2020-05-28 16:50:49 +0300

    Added unique key for Banner

--- src/modules/admob/Banner.js
+++ src/modules/admob/Banner.js
@@ -1,24 +1,28 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import AdMobComponent from './AdMobComponent';
 import { Platform } from 'react-native';
 
+let globalUniqueId = 0;
+
 function Banner({ ...props
 }) {
-  const ref = useRef()
-  useEffect(()=>{
-    return ()=> {
+  const [uniqueId, setUniqueId] = useState(globalUniqueId++);
+
+  const ref = useRef();
+  useEffect(() => {
+    return () => {
+      setUniqueId(globalUniqueId++);
       if (Platform.OS == 'ios') {
-        ref.current.destroy()
+        ref.current.destroy();
       }
-    }
-  })
-  return <AdMobComponent ref={ref} {...props} class="RNFirebaseAdMobBanner" />;
+    };
+  }, []);
+
+  return <AdMobComponent key={uniqueId} ref={ref} {...props} class="RNFirebaseAdMobBanner" />;
 }
 
 Banner.propTypes = AdMobComponent.propTypes;
-
 Banner.defaultProps = {
-  size: 'SMART_BANNER',
+  size: 'SMART_BANNER'
 };
-
 export default Banner;
